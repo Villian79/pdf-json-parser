eef4213b669cbef62012d289eae9fddc0b2d7293
Villian79 <ivilinchuk@gmail.com>
2019-01-29 20:01:01 +0200

    One more minor change

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ app.use(fileUpload());
 
 app.use(express.static(__dirname + "/public"));
 
-let resultUrl;
-
+//Main page route
 app.get('/', (req, res) => {
   res.render('index.ejs');
 });
 
+//Upload PDF file route
 app.post('/data', (req, res) => {
   
   if (Object.keys(req.files).length == 0) {
@@ -33,7 +33,6 @@ app.post('/data', (req, res) => {
 
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   let sampleFile = req.files.sampleFile;
-  console.log(sampleFile);
 
   // Use the mv() method to place the file somewhere on your server
   sampleFile.mv("img/invoice.pdf", function(err) {
@@ -46,37 +45,22 @@ app.post('/data', (req, res) => {
   res.redirect('/data');
 });
 
+//Upload File Form route
 app.get('/data/new', (req, res) => {
   res.render('input');
 })
 
-
+//JSON data display route
 app.get("/data", function(req, res) {
-    console.log(resultUrl);
-
-    // Logic for pdf2json module
-    // --------------------------------------
-    // let pdfParser = new PDFParser(this,1);
-
-    // pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
-    // pdfParser.on("pdfParser_dataReady", pdfData => {
-        // fs.writeFile(path.join(__dirname,"sample.txt"), pdfParser.getRawTextContent(), err => {
-        //   if(err) throw err;
-        //   console.log('Data has been recorded...');
-        // });
-    //     res.json(pdfData.formImage.Pages[1]);
-    // });
-
-    // pdfParser.loadPDF(path.join(resultUrl));
-
 
     //Logic for pdf-table-extracto module
     function success(result)
     {
       let resultJSON = {"guestList": []};
       let headerLength = result.pageTables[1].tables[0].length;
-      
-      for(let i = 1; i <  16; i++){
+      let guestArray = result.pageTables[1].tables;
+            
+      for(let i = 1; i <  guestArray.length - 3; i++){
         let guest = {};
         for(let j = 0; j < headerLength; j++){
           let key = result.pageTables[1].tables[0][j].split(" ").join("");
@@ -84,13 +68,10 @@ app.get("/data", function(req, res) {
           if(value != ''){
             guest[key] = value;
           }
-          
         }
 
         resultJSON.guestList.push(guest);
-        // console.log(JSON.stringify(guest));
       }
-
       
       res.json(resultJSON);
       
